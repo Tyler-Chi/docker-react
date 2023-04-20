56d14fd00267ca9dce045613c52b4ab4c4bf7588
Tyler Chi <itychi@gmail.com>
2023-04-19 21:15:18 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi there! I'm Tyler.1234
+          learn react
         </a>
       </header>
     </div>
